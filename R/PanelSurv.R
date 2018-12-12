256bbe3bab3dde3841d2b8cad6a0df1a6af67fe0
Steven Chiou <syhan.chiou@gmail.com>
2018-12-12 11:19:35 -0600

    updated help page for plot.PanelSurv

--- R/PanelSurv.R
+++ R/PanelSurv.R
@@ -64,11 +64,11 @@ is.PanelSurv <- function(x) inherits(x, "PanelSurv")
 #' Produce Tile Plot
 #'
 #' Plot the tile plot from a \code{PanelSurv} object.
-#' 
+#'
 #' @param x an object of class \code{PanelSurv}.
 #' @param heat an optional logical value indicating whether
 #' a swimmer-plot-like tile plot will be produced.
-#' @param order an optional logical value indicating whether the event plot (when \code{CSM = FALSE})
+#' @param order an optional logical value indicating whether the tile plot
 #' will be sorted by the largest observationt time.
 #' @param ... future extension
 #'
@@ -98,4 +98,4 @@ plot.PanelSurv <- function(x, heat = FALSE, order = TRUE, ...) {
                   axis.ticks.y = element_blank()) +
             ylab("Time") + xlab("Subject")
     }
-}    
+}
